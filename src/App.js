f675c67d8f815c234ad26af6e6e9e06d22901e7f
Gerardo Alvarez <jesus@conceivable.life>
2024-09-05 11:41:47 -0600

    delete unnecessaty files that corresponds to other course section behind

--- src/App.js
+++ src/App.js
@@ -1,27 +1,3 @@
-import { Component } from 'react';
-import Productos from './components/Productos'
 
-
-class App extends Component{
-  state = {
-    productos: [
-      {name: 'Tomate', price: 1500, img: '/productos/tomate.png'},
-      {name: 'Lechuga', price: 200, img: '/productos/lechuga.png'},
-      {name: 'cebolla', price: 100, img: '/productos/cebolla.png'}
-    ]
-  }
-  render(){
-    return(
-      <div>
-        <Productos
-          agregarAlCarro={()=> console.log('No hace nada')}
-          productos={this.state.productos}
-        
-        />
-        <p>HOLA MUNDO</p>
-      </div>
-    )
-  }
-}
 
 export default App;
