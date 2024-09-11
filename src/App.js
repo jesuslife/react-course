d33bf9261f7b0624d589a829c7bed169da3cc5ea
Gerardo Alvarez <jesus@conceivable.life>
2024-09-11 11:00:36 -0600

    useReducer hook

--- src/App.js
+++ src/App.js
@@ -1,55 +1,36 @@
-import { useEffect, useState } from "react";
+import { useReducer, useState } from "react";
 
-/*
-Reglas de los hooks
-    1. No se llaman en loops, ni condiciones, ni while 
-    2. solo se llaman en 2 partes:
-        - Componentes de react(functional components)
-        - custom hooks
-    3. Cuando creemos un custom hook, tiene que comenzar con use*
-*/
-const useContador = (inicial) => {
-  const [contador, setContador] = useState(inicial);
-  const incrementar = () => {
-    setContador(contador + 1);
-  };
+// const state = {contador:0}
+// action = {type: string, payload:any}   payload puede tener cualquier cosa
+const inicial = { contador: 0 };
 
-  return [contador, incrementar];
-};
-
-const Interval = ({ contador }) => {
-  useEffect(() => {
-    const i = setInterval(() => console.log(contador), 1000);
-    // desincribir sirver cuando usa conexiones a tablas o web sockets
-    return () => clearInterval(i);
-  }, [contador]);
-  return <p>Intervalo</p>;
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "incrementar":
+      return { contador: state.contador + 1 };
+    case "decrementar":
+      return { contador: state.contador - 1 };
+    case "set":
+      return { contador: action.payload };
+    default:
+      return state;
+  }
 };
 
 const App = () => {
-  /* useState retorna un arreglo
-    [0] : valor initial de useState
-    [1] : function que perimite actualizar el valor de la variable
-    */
-  const [contador, incrementar] = useContador(0);
-
-  /*
-    Use efect recibe 2 argumentos
-        1. funcion con la logica que va a ejecutar
-        2. dependencias que necesita para funcionar
-    
-        useEffect(() => {}, [])
-    */
-
-  useEffect(() => {
-    document.title = contador;
-  }, [contador]);
+  // dispatch permite despachar eventos los cuales tienen que tener la forma de action {type: string, payload:any}
+  const [state, dispatch] = useReducer(reducer, inicial);
+  const [valor, setValor] = useState(0);
 
   return (
     <div>
-      Contador: {contador}
-      <button onClick={incrementar}>Incrementar</button>
-      <Interval contador={contador} />
+      Contador: {state.contador}
+      <input value={valor} onChange={(e) => setValor(e.target.value)}></input>
+      <button onClick={() => dispatch({ type: "incrementar" })}>mas </button>
+      <button onClick={() => dispatch({ type: "decrementar" })}>menos </button>
+      <button onClick={() => dispatch({ type: "set", payload: valor })}>
+        set
+      </button>
     </div>
   );
 };
