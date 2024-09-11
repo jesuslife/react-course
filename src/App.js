b525ed486b09eb469c1feb499a72192df5233b23
Gerardo Alvarez <jesus@conceivable.life>
2024-09-11 11:57:55 -0600

    introduccion a componentes no controlados

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-function App(){
-    return (
+function App() {
+  const submit = (e) => {
+    e.preventDefault();
+    const data = Array.from(new FormData(e.target));
+    console.log(Object.fromEntries(data));
+  };
 
-    )
+  return (
+    <form onSubmit={submit}>
+      {/* <form action="/lala" method="POST"> */}
+      <div>
+        <span>lala</span>
+        <input name="campo" />
+      </div>
+      <input name="campo2" />
+      {/* submit  es como colocar un boton pero asociado con un input*/}
+      <input type="submit" value="Enviar" />
+    </form>
+  );
 }
 
 export default App;
