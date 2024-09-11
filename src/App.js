1ea633601da1cb9c1ccc7c1ce338f554b2918d04
Gerardo Alvarez <jesus@conceivable.life>
2024-09-11 10:26:58 -0600

    unknown changes due to git does not show number of lines

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {useEffect, useState} from 'react'
+import { useEffect, useState } from "react";
 
 /*
 Reglas de los hooks
@@ -9,54 +9,49 @@ Reglas de los hooks
     3. Cuando creemos un custom hook, tiene que comenzar con use*
 */
 const useContador = (inicial) => {
-    const [contador, setContador] = useState(inicial)
-    const incrementar = () => {
-        setContador(contador + 1)
-    }
+  const [contador, setContador] = useState(inicial);
+  const incrementar = () => {
+    setContador(contador + 1);
+  };
 
-    return [contador, incrementar]
-}
+  return [contador, incrementar];
+};
 
-const Interval = ({contador}) => {
-    useEffect(() => {
-        const i = setInterval(() => console.log(contador), 1000)
-        // desincribir sirver cuando usa conexiones a tablas o web sockets
-        return () => clearInterval(i)
-    }, [contador])
-    return(
-        <p>Intervalo</p>
-    )
-}
-
+const Interval = ({ contador }) => {
+  useEffect(() => {
+    const i = setInterval(() => console.log(contador), 1000);
+    // desincribir sirver cuando usa conexiones a tablas o web sockets
+    return () => clearInterval(i);
+  }, [contador]);
+  return <p>Intervalo</p>;
+};
 
 const App = () => {
-    /* useState retorna un arreglo
+  /* useState retorna un arreglo
     [0] : valor initial de useState
     [1] : function que perimite actualizar el valor de la variable
-    */ 
-    const [contador, incrementar] = useContador(0)
-    
-    /*
+    */
+  const [contador, incrementar] = useContador(0);
+
+  /*
     Use efect recibe 2 argumentos
         1. funcion con la logica que va a ejecutar
         2. dependencias que necesita para funcionar
     
         useEffect(() => {}, [])
     */
-    
-    useEffect(() =>{
-        document.title = contador
-    },[contador])
 
-    return (
-        <div>
-            Contador: {contador}
-            <button onClick={incrementar}>Incrementar</button>
-            <Interval contador = {contador}/>
+  useEffect(() => {
+    document.title = contador;
+  }, [contador]);
 
-        </div>
-    )
-}
+  return (
+    <div>
+      Contador: {contador}
+      <button onClick={incrementar}>Incrementar</button>
+      <Interval contador={contador} />
+    </div>
+  );
+};
 
-
-export default App
+export default App;
